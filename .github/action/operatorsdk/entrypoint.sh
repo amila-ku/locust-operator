6dd3ddb7cc01dcd11d68da5fbd6ce4569749b3c9
amila-ku <amila.kumaranayaka@gmail.com>
2021-05-04 00:10:46 +0200

    check if controller gen is installed

--- .github/action/operatorsdk/entrypoint.sh
+++ .github/action/operatorsdk/entrypoint.sh
@@ -6,7 +6,8 @@ mkdir -p "$PROJECT_ROOT"
 rmdir "$PROJECT_ROOT"
 ln -s "$GITHUB_WORKSPACE" "$PROJECT_ROOT"
 cd "$PROJECT_ROOT" || exit 1
+which controller-gen
 
 # Execute
-make generate
+
 make build "$@":"$VERSION"
